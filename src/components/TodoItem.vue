5d08fed88aebff8ffd874afb8a3831073efdf31b
zhangwei66 <zhangwei66@xiaomi.com>
2020-12-16 18:07:02 +0800

    feat: Todo demo

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="todo-item">
+    <li>
+      {{ title }}
+      <button @click="$emit('remove')">Remove</button>
+    </li>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["title"],
+  emits: ["remove"],
+};
+</script>
